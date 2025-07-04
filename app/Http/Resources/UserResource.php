43531859ba8133e4bcc7a81f57b59c17f297d8f5
joalisson-p-maia <namibo654@gmail.com>
2025-07-03 21:59:21 -0300

    feat: created resource to define data return

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'profile' => asset('storage/profile/' . $this->id . '/' . $this->profile),
+            'full_name' => $this->full_name,
+            'age' => $this->age,
+            'address' => [
+                'street' => $this->street,
+                'neighborhood' => $this->neighborhood,
+                'state' => $this->state,
+            ],
+            'biography' => $this->biography,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString()
+        ];
+    }
+}
