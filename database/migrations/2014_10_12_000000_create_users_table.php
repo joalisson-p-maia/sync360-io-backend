7eb37af8306fbb6e1e35326c8fff5fa9fa88342e
joalisson-p-maia <namibo654@gmail.com>
2025-07-03 21:56:53 -0300

    refactor: defining data, its types and rules, in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('profile');
+            $table->string('full_name')->nullable(false);
+            $table->string('street')->nullable(false);
+            $table->string('neighborhood')->nullable(false);
+            $table->string('state')->nullable(false);
+            $table->string('biography')->nullable(false);
             $table->timestamps();
         });
     }
