91bf2d2c57309a85c9e53cb67b0972ac4fa7be20
joalisson-p-maia <namibo654@gmail.com>
2025-07-05 19:10:14 -0300

    refactor: removed text and route, by user route group

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix("/api/user/")->group(function() {
+    Route::get("list_all_users", [UserController::class,"listAllUsers"]);
+    Route::get("get_user_by_id", [UserController::class,"getUserById"]);
+    Route::get("create_user", [UserController::class,"createUser"]);
+    Route::get("update_user_by_id", [UserController::class,"updateUserById"]);
+    Route::get("delete_user_by_id", [UserController::class,"deleteUserById"]);
 });
