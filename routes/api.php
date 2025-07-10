381eacf0ce5d83dfe682b297edef6f135f1fb461
joalisson-p-maia <namibo654@gmail.com>
2025-07-10 00:20:06 -0300

    feat: routes are created, with a prefix, and within a group

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("/api/user/")->group(function() {
+Route::prefix("/user/")->group(function() {
     Route::get("list_all_users", [UserController::class,"listAllUsers"]);
-    Route::get("get_user_by_id", [UserController::class,"getUserById"]);
-    Route::get("create_user", [UserController::class,"createUser"]);
-    Route::get("update_user_by_id", [UserController::class,"updateUserById"]);
-    Route::get("delete_user_by_id", [UserController::class,"deleteUserById"]);
+    Route::get("get_user_by_id/{id}", [UserController::class,"getUserById"]);
+    Route::post("create_user", [UserController::class,"createUser"]);
+    Route::post("update_user_by_id/{id}", [UserController::class,"updateUserById"]);
+    Route::delete("delete_user_by_id/{id}", [UserController::class,"deleteUserById"]);
 });
